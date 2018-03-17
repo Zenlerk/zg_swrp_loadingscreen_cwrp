91989bce9d2df67d8e1c42869ed2c89d0e33d97b
Zenlerk <zg.zenlerk@gmail.com>
2018-03-17 17:38:02 +1300

    loadingscreen

---
+++ sleekLoad2/js/config.js
@@ -0,0 +1,160 @@
+﻿/*    _           _    _                     _ 
+     | |         | |  | |                   | |
+  ___| | ___  ___| | _| |     ___   __ _  __| |
+ / __| |/ _ \/ _ \ |/ / |    / _ \ / _` |/ _` |
+ \__ \ |  __/  __/   <| |___| (_) | (_| | (_| |
+ |___/_|\___|\___|_|\_\______\___/ \__,_|\__,_|
+    version 2.10
+*/
+
+/*URL for the server logo
+Leave it blank to use text instead
+(Put the files in the images folder)*/
+var l_serverImage = "logo.png";
+
+/*Display current map and gamemode?
+'true' or 'false'*/
+var l_displayMapGamemode = true;
+
+/*Custom server name configuration
+Leave it blank for automatic server name*/
+var l_serverName = "sleekLoad 2";
+
+/*Use a video for the background?
+Otherwise it will use images
+'true' or 'false'*/
+var l_bgVideo = false;
+
+/*Default available videos:
+"aurora.webm"
+"bluefall.webm"
+"bluetilefloor.webm"
+"bluewhitecircles.webm"
+"bokehlens.webm"
+"cleanbokeh.webm"
+"coldnight.webm"
+"colorfulburst.webm"
+"dancingstars.webm"
+"dropsflares.webm"
+"focusorangebokeh.webm"
+"glimmering.webm"
+"glowingrings.webm"
+"greencircles.webm"
+"hues.webm"
+"inthedeep.webm"
+"modernflourish.webm"
+"orangeaura.webm"
+"warminside.webm"
+"yellowgreen.webm"
+
+You can add your own video backgrounds
+Just make sure they are using webm VP8 format 
+(Place the videos in the backgrounds/videos folder)*/
+var l_background = "bluefall.webm";
+
+/*(Image-background only)
+Enable map-based background image?
+If enabled, the background will be an image based on the server current map
+You need to have a background image with the same name as the map
+Also, the image needs to be in .jpg format
+E.g: gm_flatgrass.jpg, gm_construct.jpg, rp_downtown_v4c_v2.jpg
+'true' or 'false'*/
+var l_bgImageMapBased = false;
+
+/*(Image-background only)
+Place one or more images
+Multiple images will automatically shuffle between them
+(Place the files in the backgrounds/images folder)*/
+var l_bgImages = [
+	"1.jpg",
+	"2.jpg",
+	"3.jpg",
+	"4.jpg",
+	"5.jpg",
+	"6.jpg",
+	"7.jpg",
+	"8.jpg",
+];
+
+/*(Image-background only)
+Random background images order?
+'true' or 'false'*/
+var l_bgImagesRandom = true;
+
+/*(Image-background only)
+Delay between background images changes
+in milliseconds*/
+var l_bgImageDuration = 5000;
+
+/*(Image-background only)
+Image background fade velocity
+in milliseconds*/
+var l_bgImageFadeVelocity = 2000;
+
+/*Enable background overlay?
+'true' or 'false'*/
+var l_bgOverlay = true;
+
+/*Background darkening amount
+0 for none (0%), 100 for pitch black (100%)*/
+var l_bgDarkening = 50;
+
+/*Play music during the loading screen?
+'true' or 'false'*/
+var l_music = true;
+
+/*Display current music name?
+'true' or 'false'*/
+var l_musicDisplay = true;
+
+/*Music playlist
+Add as many youtube ids or ogg files you want
+(Place the ogg files in the music folder)*/
+var l_musicPlaylist = [
+	{ogg: "swrp1.ogg", name: "ZG | Loading Screen Song"},
+	//{youtube: "zA6wRutN_7Q", name: "Suwa-cafeteria"},
+];
+
+/*Random music order?
+'true' or 'false'*/
+var l_musicRandom = true;
+
+/*Music volume
+Choose a value between 0 (quietest) and 100 (loudest)*/
+var l_musicVolume = 15;
+
+/*Enable custom messages?
+'true' or 'false'*/
+var l_messagesEnabled = true;
+
+/*Enter your custom messages below*/
+var l_messages = [
+	"Enjoy the server",
+	"Thanks for joining us!",
+	"Please make sure to join our Discord!",
+	"Please make sure to check out our Website and Forums!",
+	"Invite friends to have more fun on the server.",
+	"Most of our music is provided by NCS on YouTube.",
+	"This loading screen was purchased on gmodstore."
+];
+
+/*Random message order?
+'true' or 'false'*/
+var l_messagesRandom = true;
+
+/*Delay between message changes
+in milliseconds*/
+var l_messagesDelay = 5000;
+
+/*Messages fade time
+in milliseconds*/
+var l_messagesFade = 1000;
+
+//==============================================================
+//========================== WARNING ===========================
+//==============================================================
+//============ Do not edit anything below this line ============
+//==============================================================
+var checkConfigFile = function() {
+	return true;
+};
